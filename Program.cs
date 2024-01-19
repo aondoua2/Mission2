2e30e45352619805ab62efc423ae1de10a7d8135
Ashley Ondoua <aondoua2@Byu.edu>
2024-01-19 07:47:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Ashley Ondoua
+// Section 4
+// Mission #2 assignment-- dice throwing simulator
+using HW1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Welcome to the dice throwing simulator!
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        // Allow the user to choose how many times the “dice” will be thrown
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        int rollNumber = int.Parse(Console.ReadLine()); // Get user input for rollNumber
+
+        // i want to connect to the other file (SecondClass)
+        SecondClass sc = new SecondClass();
+        // Do I need an instance of the second class?
+        // SecondClass SecondClassInstance = new SecondClass();
+
+
+        // Calling the method in SecondClass to pass the number of rolls
+        int[] resultsArray = sc.rollingTheDice(rollNumber);
+
+        // In the first class, use that array to print a histogram
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+
+        // Total number of rolls
+        System.Console.WriteLine("Total number of rolls = " + rollNumber);
+
+        for (int i = 2; i <= 12; i++)
+        {
+            // Calculate the percentage of times that number was rolled
+            int percentage = (resultsArray[i] * 100) / rollNumber;
+
+            // Print the number and the corresponding percentage as asterisks
+            string asterisks = new string('*', percentage);
+            Console.WriteLine(i + ": " + asterisks);
+        }
+
+        // Thank you for using the dice throwing simulator. Goodbye!
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ SecondClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    // SecondClass definition
+    public class SecondClass
+        {
+            // Method to simulate the roll of the dice for the specified number of times
+            public int[] rollingTheDice(int rollNumber)
+            {
+                // Array to keep track of the number of times that each combination is thrown
+                int[] myArray = new int[13];
+
+                // Variables to represent the two dice and their total
+                int die1 = 0;
+                int die2 = 0;
+                int total = 0;
+
+                // Random number generator for dice rolls
+                Random random = new Random();
+
+                // Loop through the specified number of rolls
+                for (int roll = 0; roll < rollNumber; roll++)
+                {
+                    // Simulate the roll of two dice
+                    die1 = random.Next(1, 7);
+                    die2 = random.Next(1, 7);
+                    total = die1 + die2;
+
+                    // Update the array based on the dice roll
+                    myArray[total]++;
+                }
+
+                // Return the array after all rolls
+                return myArray;
+            }
+        }
+    }
+
+
